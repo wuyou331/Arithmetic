d103af8b962f9046e252071a0b18a8ccbeb70dd0
wuyou <wuyou331@gmail.com>
2018-08-04 11:47:03 +0800

    优化代码

--- Arithmetic/Calculator.cs
+++ Arithmetic/Calculator.cs
@@ -22,58 +22,20 @@ namespace Arithmetic
             var signStack = new Stack<char>();
             var index = 0;
 
-
-            var first = NumberParse.TryParse(expr);
-            index += first.Remainder.Position;
-            numberStack.Push(Convert.ToDouble(first.Value));
+            numberStack.Push(GetNextValue(expr, ref index));
             while (index < expr.Length)
             {
                 var sign = SignParse.TryParse(expr.Substring(index));
                 index += sign.Remainder.Position;
-                var number = 0d;
-                var numberResult = NumberParse.TryParse(expr.Substring(index));
-                if (numberResult.WasSuccessful)
-                {
-                    index += numberResult.Remainder.Position;
-                    number = Convert.ToDouble(numberResult.Value);
-                }
-                else
-                {
-                    var bracketStack = new Stack<int>();
-                    var start = 0;
-                    var end = 0;
-                    do
-                    {
-                        var bracket = BracketsParser.TryParse(expr.Substring(index));
-                        if (bracket.WasSuccessful)
-                        {
-                            index += bracket.Remainder.Position;
-                            if (bracket.Value == '(')
-                            {
-                                bracketStack.Push(index);
-                            }
-                            else if (bracket.Value == ')')
-                            {
-                                start = bracketStack.Pop();
-                                end = index - start - bracket.Remainder.Position;
-                            }
-                        }
-                        else
-                        {
-                            index += 1;
-                        }
-                    } while (bracketStack.Count > 0);
 
-                    var sub = expr.Substring(start, end);
-                    number = Sum(sub);
-                }
+                var number = GetNextValue(expr,ref index);
 
                 if (sign.Value == '+' || sign.Value == '-')
                 {
+                    //当符号为+ -时，优先计算出栈中的内容，并将结果入栈
                     if (numberStack.Count > 1)
                     {
-                        var firstSum = SumStack(numberStack, signStack);
-                        numberStack.Push(firstSum);
+                        SumStack(numberStack, signStack);
                     }
 
                     signStack.Push(sign.Value);
@@ -93,14 +55,77 @@ namespace Arithmetic
                     numberStack.Push(result);
                 }
             }
+            SumStack(numberStack, signStack);
 
-
-            return SumStack(numberStack, signStack);
+            return numberStack.Pop();
         }
 
-        private double SumStack(Stack<double> numberStack, Stack<char> signStack)
+        /// <summary>
+        /// 获取下一个值，如果是表达式，计算出结果返回
+        /// </summary>
+        /// <param name="expr"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        private double GetNextValue(string expr, ref int index)
         {
+            var first = NumberParse.TryParse(expr);
+            if (first.WasSuccessful)
+            {
+                index += first.Remainder.Position;
+                return Convert.ToDouble(first.Value);
+            }
+            else
+            {
+                var subExpr = SubExprString(expr, ref index);
+                return Sum(subExpr);
+            }
+        }
 
+        /// <summary>
+        /// 截取子表达式
+        /// </summary>
+        /// <param name="expr"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        private string SubExprString(string expr, ref int index)
+        {
+            //递归计算出括号中的运算符
+            var bracketStack = new Stack<int>();
+            //计算括号内容的开始和结尾，用于截取完整的子表达式
+            var start = 0;
+            var end = 0;
+            do
+            {
+                var bracket = BracketsParser.TryParse(expr.Substring(index));
+                if (bracket.WasSuccessful)
+                {
+                    index += bracket.Remainder.Position;
+                    if (bracket.Value == '(')
+                    {
+                        bracketStack.Push(index);
+                    }
+                    else if (bracket.Value == ')')
+                    {
+                        start = bracketStack.Pop();
+                        end = index - start - bracket.Remainder.Position;
+                    }
+                }
+                else
+                {
+                    index += 1;
+                }
+            } while (bracketStack.Count > 0);
+            return expr.Substring(start, end);
+        }
+
+        /// <summary>
+        /// 计算栈中的内容
+        /// </summary>
+        /// <param name="numberStack"></param>
+        /// <param name="signStack"></param>
+        /// <returns></returns>
+        private void SumStack(Stack<double> numberStack, Stack<char> signStack)
+        {
             while (numberStack.Count > 1)
             {
                 var right = numberStack.Pop();
@@ -109,7 +134,6 @@ namespace Arithmetic
                 numberStack.Push(sign == '+' ? right + left : left - right);
             }
 
-            return numberStack.Pop();
         }
     }
 }
